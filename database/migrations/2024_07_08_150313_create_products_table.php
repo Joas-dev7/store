c19f4d40a004b6f8754de83c9dfd02a9731eb3f6
Joas-dev7 <lozjoas.dev@gmail.com>
2024-07-08 11:53:10 -0400

    fake data and db correction

--- database/migrations/2024_07_08_150313_create_products_table.php
+++ database/migrations/2024_07_08_150313_create_products_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('products', function (Blueprint $table) {
             $table->id();
             $table->foreignId('category_id')->constrained()->cascadeOnDelete();
-            $table->string('nom', 50);
+            $table->string('name', 50);
             $table->string('description');
             $table->string('price');
             $table->json('images')->nullable();
