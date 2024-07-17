44f5461e85140b3a4c90b21f24db8af6d0603891
Joas-dev7 <lozjoas.dev@gmail.com>
2024-07-17 10:04:12 -0400

    layout et panier

---
+++ app/Http/Controllers/PanierController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PanierController extends Controller
+{
+    public function index()
+    {
+        return 'liste';
+    }
+
+    public function ajouter()
+    {
+        return 'ajouter';
+    }
+
+    public function commander()
+    {
+        return 'commander';
+    }
+}
